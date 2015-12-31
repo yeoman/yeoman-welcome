9dbdb5dcbfe68d1b703c97f1aff7464f9bfb37ca
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 12:44:53 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
-'use strict';
-var test = require('ava');
-var yeomanWelcome = require('./');
+import test from 'ava';
+import x from './';
 
-test(function (t) {
-	console.log(yeomanWelcome);
-	t.assert(yeomanWelcome.length > 0);
-	t.assert(yeomanWelcome.indexOf('Welcome to Yeoman') !== -1);
-	t.end();
+test(t => {
+	t.true(x.length > 0);
+	t.true(x.indexOf('Welcome to Yeoman') !== -1);
 });
