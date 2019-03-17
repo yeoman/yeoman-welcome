7e18e1abaab5a1979eb255240f81e763380960b4
KoolTeba <26543236+KoolTheba@users.noreply.github.com>
2019-03-17 15:14:43 +0100

    Migration to ES6

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
-var chalk = require('chalk');
+const chalk = require('chalk'),
+    r = chalk.red,
+    yB = chalk.yellow.bold,
+    y = chalk.yellow;
 
 module.exports =
-'\n     _-----_' +
-'\n    |       |' +
-'\n    |' + chalk.red('--(o)--') + '|   .--------------------------.' +
-'\n   `---------´  |    ' + chalk.yellow.bold('Welcome to Yeoman,') + '    |' +
-'\n    ' + chalk.yellow('(') + ' _' + chalk.yellow('´U`') + '_ ' + chalk.yellow(')') + '   |   ' + chalk.yellow.bold('ladies and gentlemen!') + '  |' +
-'\n    /___A___\\   \'__________________________\'' +
-'\n     ' + chalk.yellow('|  ~  |') +
-'\n   __' + chalk.yellow('\'.___.\'') + '__' +
-'\n ´   ' + chalk.red('`  |') + '° ' + chalk.red('´ Y') + ' `\n';
+`
+    _-----_
+   |       |
+   |${r("--(o)--")}|   .--------------------------.
+  \`---------\´  |    ${yB("Welcome to Yeoman,")}    |
+   ${y("(")} _\´${y("U")}\`_ ${y(")")}   |   ${yB("ladies and gentlemen!")}  |
+   /___A___\\   '__________________________'
+    ${y("|  ~  |")}
+  __${y("'.___.'")}__
+\´   ${r("`  |")}° ${r("´ Y")} \``
